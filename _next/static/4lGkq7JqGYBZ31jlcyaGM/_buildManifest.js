489f2f70d185476a9f52564ff6552c68e7e52977
Redskull-127 <76103614+Redskull-127@users.noreply.github.com>
2022-11-10 13:28:14 +0000

    Deploying to gh-pages from @ Redskull-127/roses@d0ac2ad49ef561cecfc87b3f8e02f8a234d55712 🚀

--- _next/static/4lGkq7JqGYBZ31jlcyaGM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a52ff0026a55d91c.css","static/chunks/pages/index-c96685c0827e66d8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Jhery63fSWujh5BYV_IFQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/876-41dc63fc61796d97.js","static/css/a7710b650b3325f0.css","static/chunks/pages/index-37dc6c8823f303c9.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
